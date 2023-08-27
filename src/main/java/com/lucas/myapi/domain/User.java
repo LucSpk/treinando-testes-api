4531d36b8715dca9c33e678d885939a448b69f17
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-08-27 16:52:22 -0300

    Iniciando projeto

---
+++ src/main/java/com/lucas/myapi/domain/User.java
@@ -0,0 +1,70 @@
+package com.lucas.myapi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty(message = "Campo NOME é Mandatório")
+    @Length(min = 10, max = 150, message = "O nome deve ter entre 10 e 150 Caracteres")
+    private String nome;
+    @NotEmpty(message = "Campo LOGIM é Mandatório")
+    private String login;
+    @NotEmpty(message = "Campo SENHA é Mandatório")
+    private String senha;
+
+    public User() {
+    }
+
+    public User(Integer id, String nome, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
